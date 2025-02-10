1d7890b3bb899f38396761b38fa54a90096b9c8c
Deeeeya <159756541+Deeeeya@users.noreply.github.com>
2025-02-10 17:01:30 -0500

    in-class-8

---
+++ in-cass-8/main.js
@@ -0,0 +1,12 @@
+// let num = 10;
+// let word = "Light";
+// let result = num + word;
+
+// console.log(result);
+
+// console.log(Number(false));
+// console.log(Number(true));
+
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean("0"));
